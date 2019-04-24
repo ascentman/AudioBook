f0ecc11e0a0ef28fbfd575814aa8e873376ee6ef
Volodymyr Rykhva <ascentman@ukr.net>
2019-04-25 00:49:37 +0300

    add a little animation on scroll

--- AudioBook/Modules/Home/HomeDataProvider.swift
+++ AudioBook/Modules/Home/HomeDataProvider.swift
@@ -14,7 +14,12 @@ final class HomeDataProvider: NSObject {
 
 }
 
+// MARK: - Extensions
+
 extension HomeDataProvider: UICollectionViewDataSource {
+
+    // MARK: - UICollectionViewDataSource
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if selectedSegment == 0 {
             return dataManager.new.count
@@ -35,7 +40,28 @@ extension HomeDataProvider: UICollectionViewDataSource {
     }
 }
 
+extension HomeDataProvider: UICollectionViewDelegate {
+
+    // MARK: - UICollectionViewDelegate
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        cell.alpha = 0
+        cell.layer.transform = CATransform3DMakeScale(0.8, 0.8, 0.8)
+
+        UIView.animate(withDuration: 0.3, animations: {
+            cell.alpha = 1
+            cell.layer.transform = CATransform3DScale(CATransform3DIdentity, 1, 1, 1)
+        })
+    }
+}
+
 extension HomeDataProvider: UICollectionViewDelegateFlowLayout{
+
+    // MARK: - UICollectionViewDelegateFlowLayout
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let padding: CGFloat = 10
