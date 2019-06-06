b39f07ac350da9e43499140a5b1a2a56e021f4d7
Volodymyr Rykhva <volodymyr.rykhva@sigma.software>
2019-06-07 02:49:36 +0300

    small ui fixes

--- AudioBook/Scenes/About/AboutViewController.swift
+++ AudioBook/Scenes/About/AboutViewController.swift
@@ -11,12 +11,21 @@ import UIKit
 final class AboutViewController: UIViewController {
 
     @IBOutlet weak var generalTextView: UITextView!
+    @IBOutlet weak var donateButton: UIButton!
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupNavigationBar()
         generalTextView.alwaysBounceVertical = true
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+         animateDonateButton()
     }
 
     // MARK: - Private
@@ -26,5 +35,14 @@ final class AboutViewController: UIViewController {
         navigationController?.navigationBar.shadowImage = UIImage()
         let attributes = [NSAttributedString.Key.font: UIFont(name: "Charter", size: 22)!]
         navigationController?.navigationBar.titleTextAttributes = attributes
+    }
+
+    private func animateDonateButton() {
+        UIView.animate(withDuration: 0.5) { [weak self] in
+            self?.donateButton.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        }
+        UIView.animate(withDuration: 0.5, delay: 0.3, options: UIView.AnimationOptions.curveEaseInOut, animations: { [weak self] in
+            self?.donateButton.transform = CGAffineTransform(rotationAngle: CGFloat.pi * 2.0)
+        }, completion: nil)
     }
 }
